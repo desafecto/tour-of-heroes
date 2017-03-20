1d2d541f6128d1a243b62d5aad62868ec839e526
desafecto <eldesafecto@gmail.com>
2017-03-20 23:46:43 +0100

    service injection

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { Hero } from './hero';
+
+@Component({
+  selector: 'my-hero-detail',
+  template: `
+    <div *ngIf="heroInput">
+      <h2>{{heroInput.name}} details!</h2>
+      <div><label>id: </label>{{heroInput.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="heroInput.name" placeholder="name"/>
+      </div>
+      <div><label>price: $</label>{{heroInput.price}}</div>
+    </div>
+  `
+})
+export class HeroDetailComponent {
+
+  @Input()
+  heroInput: Hero;
+}
---
+++ app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 10, name: 'Windstorm', price: 0 },
+  { id: 11, name: 'Mr./Mss. Nice', price: 0 },
+  { id: 12, name: 'Narco galego', price: 0 },
+  { id: 13, name: 'Bombasto', price: 0 },
+  { id: 14, name: 'Celeritas', price: 0 },
+  { id: 15, name: 'Magn-eta', price: 0 },
+  { id: 16, name: 'RubberMan', price: 0 },
+  { id: 17, name: 'Dynama', price: 0 },
+  { id: 18, name: 'Dr IQ', price: 0 },
+  { id: 19, name: 'Magma mia', price: 0 },
+  { id: 20, name: 'Tornado', price: 0 }
+];
